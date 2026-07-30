13483b5e40edcc9e828c89c860c54adf0424e7d3
A7ayat2009-cyber <A7ayat2009@icloud.com>
2026-07-30 04:38:24 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
 const firebaseConfig = {
   apiKey: "...",
   authDomain: "...",
